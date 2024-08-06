6f58bef49ab5a7b6aca411156608edbf8c8cf0b0
Chitl1 <thanlinhchi010995@gmail.com>
2024-08-06 17:49:03 +0700

    Bai tập Chương 8

---
+++ Chuong8/Chuong8/Program.cs
@@ -0,0 +1,55 @@
+﻿//Bài 1
+//EmployeeDetails objDetails = new EmployeeDetails();
+//objDetails.EmpName = "Bill";
+//objDetails.SetId(10);
+//Console.WriteLine("Employee Name:" + objDetails.EmpName);
+//Console.WriteLine("Employee Id:" + objDetails.EmpId);
+//class EmployeeDetails
+//{
+//    private string _empName;
+//    private int _empId;
+//    public string EmpName
+//    {
+//        get
+//        {
+//            return _empName;
+//        }
+//        set
+//        {
+//            if(value !="")
+//            _empName = value;
+//        }
+//    }
+//    public void SetId(int val)
+//    {
+//        _empId = val;
+//    }
+//    public int EmpId
+//    {
+//        get
+//        {
+//            return _empId;
+//        }
+//    }
+//} 
+
+
+////bài 2
+//Balance objBal = new Balance();
+//objBal.BalanceAmount = 1005.50;
+//Console.WriteLine(objBal.BalanceAmount);
+//class Balance
+//{
+//    private double _balanceAmount;
+//    public double BalanceAmount
+//    {
+//        get
+//        {
+//            return _balanceAmount;
+//        }
+//        set
+//        {
+//            _balanceAmount = value;
+//        }
+//    }
+//}
