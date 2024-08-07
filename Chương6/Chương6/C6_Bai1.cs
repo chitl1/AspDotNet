a9de8fcd201f7f4ffc462312cbd4bc3e5bd21185
Chitl1 <thanlinhchi010995@gmail.com>
2024-08-07 10:49:02 +0700

    sửa cấu trúc thư mục code và code

--- Chương6/Chương6/C6_Bai1.cs
+++ Chương6/Chương6/C6_Bai1.cs
@@ -118,5 +118,112 @@ namespace Chương6
                 return $"Square(bottomLeft={BottomLeft}, sideLength={Width})";
             }
         }
+        public static double GetSideLength(Diem p1, Diem p2)
+        {
+            return Math.Sqrt(Math.Pow(p2.x - p1.x, 2) + Math.Pow(p2.y - p1.y, 2));
+        }
+        public class Triangle : Shape
+        {
+            public Diem d1 { get; set; }
+            public Diem d2 { get; set; }
+            public Diem d3 { get; set; }
+            public Triangle(Diem point1, Diem point2, Diem point3)
+            {
+                d1 = point1;
+                d2 = point2;
+                d3 = point3;
+            }
+            public override double Dientich()
+            {
+                double a = GetSideLength(d1, d2);
+                double b = GetSideLength(d2, d3);
+                double c = GetSideLength(d3, d1);
+                double s = (a + b + c) / 2;
+                return Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+            }
+            public override double Chuvi()
+            {
+                double a = GetSideLength(d1, d2);
+                double b = GetSideLength(d2, d3);
+                double c = GetSideLength(d3, d1);
+                return a + b + c;
+            }
+            public override string ToString()
+            {
+                return $"Triangle(d1: {d1}, d2: {d2}, d3: {d3})";
+            }
+        }
+        
+        // Hình bình hành
+        public class Parallelogram : Shape
+        {
+            public Diem d1 { get; set; }
+            public Diem d2 { get; set; }
+            public Diem d3 { get; set; }
+            public Diem d4 { get; set; }
+
+            public Parallelogram(Diem _d1, Diem _d2, Diem _d3, Diem _d4)
+            {
+                d1 = _d1;
+                d2 = _d2;
+                d3 = _d3;
+                d4 = _d4;
+            }
+
+            public override double Dientich()
+            {
+                double baseLength = GetSideLength(d1, d2);
+                double height = Math.Abs(d3.y - d1.y);
+                return baseLength * height;
+            }
+
+            public override double Chuvi()
+            {
+                double a = GetSideLength(d1, d2);
+                double b = GetSideLength(d2, d3);
+                return 2 * (a + b);
+            }
+
+            public override string ToString()
+            {
+                return $"Parallelogram(d1: {d1}, d2: {d2}, d3: {d3}, d4: {d4})";
+            }
+        }
+
+        // Hình thoi
+        public class Rhombus : Shape
+        {
+            public Diem d1 { get; set; }
+            public Diem d2 { get; set; }
+            public Diem d3 { get; set; }
+            public Diem d4 { get; set; }
+
+            public Rhombus(Diem _d1, Diem _d2, Diem _d3, Diem _d4)
+            {
+                d1 = _d1;
+                d2 = _d2;
+                d3 = _d3;
+                d4 = _d4;
+            }
+
+            public override double Dientich()
+            {
+                double diagonal1 = GetSideLength(d1, d3);
+                double diagonal2 = GetSideLength(d2, d4);
+                return (diagonal1 * diagonal2) / 2;
+            }
+
+            public override double Chuvi()
+            {
+                double sideLength = GetSideLength(d1, d2);
+                return 4 * sideLength;
+            }
+
+            public override string ToString()
+            {
+                return $"Rhombus(d1: {d1}, d2: {d2}, d3: {d3}, d4: {d4})";
+            }
+        }
+
     }
 }
